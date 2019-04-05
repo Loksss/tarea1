edf8bf691f181695cdd131f613d85c73276db3cc
Ronalok <ronalokd@gmail.com>
2019-04-05 14:37:42 -0400

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+function uno(){
+    var matrix = [[0,1,1,2],[0,5,0,0],[2,0,3,3]];
+    var sum=0;
+    for(var i=0;i<3;i++){
+        for(var j=0;j<4;j++){
+            sum+=matrix[i][j];
+        }
+    }
+    return sum;
+}
+console.log("ejercicio 1  [[0,1,1,2],[0,5,0,0],[2,0,3,3]] =  "+uno());
